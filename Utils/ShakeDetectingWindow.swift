15182971e2f3dd8f987c2598583a606651c72faf
Br3nd4nt <Vlad.belov.08@gmail.com>
2025-06-28 02:05:29 +0300

    some unstaged files

---
+++ Utils/ShakeDetectingWindow.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class ShakeDetectingWindow: UIWindow {
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        super.motionEnded(motion, with: event)
+        if motion == .motionShake {
+            NotificationCenter.default.post(name: .deviceDidShakeNotification, object: nil)
+        }
+    }
+} 
