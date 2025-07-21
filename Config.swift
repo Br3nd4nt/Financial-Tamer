fbe327c3d796be53d53d2132e748ee9b1388ebfb
Br3nd4nt <Vlad.belov.08@gmail.com>
2025-07-21 18:25:39 +0300

    config file fix

---
+++ Config.swift
@@ -0,0 +1,23 @@
+//
+//  Config 2.swift
+//  Financial Tamer
+//
+//  Created by br3nd4nt on 19.07.2025.
+//
+
+
+import Foundation
+
+// swiftlint:disable all
+enum Config {
+    static let bearerToken = "TOKEN"
+    
+    static let baseURL = "https://shmr-finance.ru/api"
+    
+    static let apiVersion = "v1"
+     
+    static var apiBaseURL: URL {
+        return URL(string: "\(baseURL)/\(apiVersion)")!
+    }
+} 
+// swiftlint:enable all
