9cf6dfddf336cb058aa2e124de66332fe74408b4
Br3nd4nt <Vlad.belov.08@gmail.com>
2025-07-23 11:03:05 +0300

    config fix

--- Config.template.swift
+++ Config.template.swift
@@ -1,14 +1,16 @@
 //
-//  Config.template.swift
+//  Config.swift
 //  Financial Tamer
 //
-//  Created by br3nd4nt on 19.07.2025.
+//  Created by br3nd4nt on 23.07.2025.
 //
 
 import Foundation
 
+// Rename the enum to "Config" and input your token
+
 // swiftlint:disable all
-enum Config {
+enum Config_template {
     static let bearerToken = "YOUR_API_KEY_HERE"
     
     static let baseURL = "https://shmr-finance.ru/api"
